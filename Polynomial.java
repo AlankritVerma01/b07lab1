b926e9fe20514564f757e5c8bc2fa28134a71abc
AlankritVerma01 <alankrit.verma@mail.utoronto.ca>
2023-06-06 21:13:23 -0400

    in-progress for Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,47 +1,132 @@
 import java.lang.Math;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
 
 public class Polynomial{
-	double[] polynomial;
+	double[] non_zero_coff;
+	int[] powers;
 	public Polynomial()
 	{
-		polynomial = new double[1];
-		polynomial[0] = 0;
+		non_zero_coff = new double[1];
+		non_zero_coff[0] = 0;
+
+		powers = new int[1];
+		powers[0] = 0;
 	}
 	
-	public Polynomial(double arr[])
+	public Polynomial(double coff[], int power[])
 	{
-		polynomial = arr;
+		non_zero_coff = coff;
+		powers = power;
 	}
-	
+
+	public Polynomial(File f) throws IOException
+	{
+		BufferedReader reader = new BufferedReader(new FileReader(f));
+		String line = reader.readLine();
+		String[] parts = line.split("[\\+\\-]");
+		double[] non_zero_coff = new double[parts.length];
+		int[] powers = new int[parts.length];
+		int i = 0;
+		for(String part:parts)
+		{
+			String[] comp=part.split("x");
+			if(comp.length == 2)
+			{
+				non_zero_coff[i]=Double.parseDouble(comp[0]);
+				powers[i]=Integer.parseInt(comp[1]);
+			}
+			if(comp.length == 1)
+			{
+				non_zero_coff[i]=Double.parseDouble(comp[0]);
+			}
+		}
+	}
 	
 	public Polynomial add(Polynomial other)
 	{
-		double[] arr = new double[Math.max(polynomial.length, other.polynomial.length)];
-		Polynomial p = new Polynomial(arr);
-		for(int i=0; i < Math.max(other.polynomial.length, polynomial.length); i++)
+		int len = 0;
+		int highest_power = 0;
+		for(int i = 0, inner_index=0, outer_index=0; i < Math.max(powers.length, other.powers.length); i++)
 		{
-			if(i<polynomial.length && i < other.polynomial.length)
+			if(i==powers[inner_index] && i==other.powers[outer_index])
 			{
-				p.polynomial[i] = other.polynomial[i] + polynomial[i];
+				len++;
+				inner_index++;
+				outer_index++;
+				if (i>highest_power)
+				{
+					highest_power=i;
+				}
 			}
-			else if(i<polynomial.length && i >= other.polynomial.length)
+			else if(i==powers[inner_index])
 			{
-				p.polynomial[i] = polynomial[i];
+				len++;
+				inner_index++;
+				if (i>highest_power)
+				{
+					highest_power=i;
+				}
 			}
-			else if(i>=polynomial.length && i < other.polynomial.length)
+			else if(i==other.powers[outer_index])
 			{
-				p.polynomial[i] = other.polynomial[i];
+				len++;
+				outer_index++;
+				if (i>highest_power)
+				{
+					highest_power=i;
+				}
 			}
 		}
+
+		double[] sum_coff = new double[len];
+		int[] sum_power = new int[len];
+
+		int mains_power = 0;
+		int inner_index = 0;
+		int outer_index = 0;
+		int sum_index = 0;
+		while(mains_power <= highest_power)
+		{
+
+			if((mains_power == powers[inner_index]) && (mains_power == other.powers[outer_index]))
+			{
+				sum_coff[sum_index] = non_zero_coff[inner_index] + other.non_zero_coff[outer_index];
+				sum_power[sum_index] = powers[inner_index];
+				sum_index++;
+				inner_index++;
+				outer_index++;
+			}
+			else if(mains_power == powers[inner_index])
+			{
+				sum_coff[sum_index] = non_zero_coff[inner_index];
+				sum_power[sum_index] = powers[inner_index];
+				sum_index++;
+				inner_index++;
+			}
+			else if(mains_power == other.powers[outer_index])
+			{
+				sum_coff[sum_index] = other.non_zero_coff[outer_index];
+				sum_power[sum_index] = other.powers[outer_index];
+				sum_index++;
+				outer_index++;
+			}
+			mains_power++;
+		}
+
+		Polynomial p = new Polynomial(sum_coff, sum_power);
 		return p;
 	}
 	
 	public double evaluate(double x)
 	{
 		double val = 0;
-		for(int i = 0; i < polynomial.length; i++)
+		for(int i = 0; i < powers.length; i++)
 		{
-			val += polynomial[i]*Math.pow(x,i);
+			val += non_zero_coff[i]*Math.pow(x, powers[i]);
 		}
 		return val;
 	}
@@ -51,4 +136,17 @@ public class Polynomial{
 		double val = evaluate(x);
 		return (val == 0);
 	}
+
+	// public Polynomial multiply(Polynomial other)
+	// {
+	// 	true;
+	// }
+
+	public void saveToFile(String file_name)
+	{
+		String a = "";
+		
+	}
+
+
 }
